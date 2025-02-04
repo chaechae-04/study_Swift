5e6e4b0d35ff3cb46b48a287d80476e28c7bc7ac
lee0su <leeys4903@naver.com>
2025-02-04 20:35:44 +0900

    add : Splash View Animation

--- Hello_Swift/Hello_SwiftApp.swift
+++ Hello_Swift/Hello_SwiftApp.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct Hello_SwiftApp: App {
     
     @State private var showingSplash: Bool = true
+    @State private var splashOffset: CGFloat = 0
     
     var body: some Scene {
         WindowGroup {
@@ -21,10 +22,21 @@ struct Hello_SwiftApp: App {
                     .animation(.easeInOut(duration: 0.8), value: showingSplash)
                 
                 SplashView()
+                    .offset(y: splashOffset)
                     .opacity(showingSplash ? 1 : 0)
-                    .animation(.easeInOut(duration: 0.8), value: showingSplash)
+                    .animation(.easeInOut(duration: 0.8), value: splashOffset)
                     .onAppear {
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 3) { showingSplash = false }
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                            withAnimation(.easeOut(duration: 0.5)) { splashOffset = UIScreen.main.bounds.height * 0.12
+                                
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+                                    withAnimation(.spring()) {
+                                        splashOffset = -UIScreen.main.bounds.height
+                                        showingSplash = false
+                                    }
+                                }
+                            }
+                        }
                     }
             }
             
